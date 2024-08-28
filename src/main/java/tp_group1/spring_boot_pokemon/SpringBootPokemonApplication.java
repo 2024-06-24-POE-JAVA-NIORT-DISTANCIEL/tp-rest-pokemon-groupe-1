8a9e1d3239a0aa59c8c335620c12cf65fb28132a
Michael Descorcier <mdescorcier-sti@avista.fr>
2024-08-28 11:04:43 +0200

    initializer-spring-boot

---
+++ src/main/java/tp_group1/spring_boot_pokemon/SpringBootPokemonApplication.java
@@ -0,0 +1,13 @@
+package tp_group1.spring_boot_pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPokemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tp_group1/spring_boot_pokemon/SpringBootPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package tp_group1.spring_boot_pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
