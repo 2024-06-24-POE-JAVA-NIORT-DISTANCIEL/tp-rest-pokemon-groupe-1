c20124976262ba33d90812f48273820d0d23b7e1
Muriel <muriel.marchal@hotmail.com>
2024-08-28 15:04:12 +0200

    nouveaux test

--- src/test/java/tp_group1/spring_boot_pokemon/TestTrainerDao.java
+++ src/test/java/tp_group1/spring_boot_pokemon/TestTrainerDao.java
@@ -17,6 +17,20 @@ public class TestTrainerDao {
         Trainer trainer1 = new Trainer(null, "Ivy", "12", 100);
         trainerDao.save(trainer1);
         Assertions.assertEquals(1,trainer1.getId());
+    }
 
+    @Test
+    public void testFindById() {
+        Trainer trainer1 = new Trainer(1L, "Ivy", "12", 100);
+        trainerDao.findById(trainer1.getId());
+        Assertions.assertEquals(1L, trainer1.getId());
+    }
+
+    @Test
+    public void testDeleteById() {
+        Trainer trainer1 = new Trainer(1L, "Ivy", "12", 100);
+        Trainer trainerSaved = trainerDao.save(trainer1);
+        trainerDao.deleteById(trainerSaved.getId());
+        Assertions.assertTrue(trainerDao.findById(trainer1.getId()).isEmpty());
     }
 }
