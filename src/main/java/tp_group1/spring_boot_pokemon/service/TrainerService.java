ff9ba5a905b01eba4f12ca74298ae71ad2e4890f
Muriel <muriel.marchal@hotmail.com>
2024-08-29 14:23:11 +0200

    add service trainer

--- src/main/java/tp_group1/spring_boot_pokemon/service/TrainerService.java
+++ src/main/java/tp_group1/spring_boot_pokemon/service/TrainerService.java
@@ -1,11 +1,41 @@
 package tp_group1.spring_boot_pokemon.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import tp_group1.spring_boot_pokemon.dao.TrainerDao;
+import tp_group1.spring_boot_pokemon.model.Trainer;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TrainerService {
-//    @Autowired
-//    private TrainerDao trainerDao;
+    @Autowired
+    private TrainerDao trainerDao;
+
+    //methode save
+    @Transactional
+    public Trainer save(Trainer trainer) {
+        Trainer trainerSaved = this.trainerDao.save(trainer);
+        return trainerSaved;
+    }
+
+    //methode find by
+    public Trainer findById(Long id) {
+        Optional<Trainer> optionalTrainer = this.trainerDao.findById(id);
+        return optionalTrainer.orElse(null);
+    }
+
+    //methode pour trouver tous les trainers
+    public List<Trainer> findAll() {
+        return (List<Trainer>) this.trainerDao.findAll();
+    }
+
+    //delete by id
+    @Transactional
+    public void deleteById(Long id) {
+        this.trainerDao.deleteById(id);
+    }
+
 }
