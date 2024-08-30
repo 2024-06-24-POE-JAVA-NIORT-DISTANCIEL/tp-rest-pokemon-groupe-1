f97ff7760cc0791fce4ae21a50a513b64d2b2618
Muriel <muriel.marchal@hotmail.com>
2024-08-30 08:57:36 +0200

    add service attack

--- src/main/java/tp_group1/spring_boot_pokemon/service/AttackService.java
+++ src/main/java/tp_group1/spring_boot_pokemon/service/AttackService.java
@@ -1,11 +1,39 @@
 package tp_group1.spring_boot_pokemon.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tp_group1.spring_boot_pokemon.dao.AttackDao;
+import tp_group1.spring_boot_pokemon.model.Attack;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AttackService {
     @Autowired
     private AttackDao attackDao;
+
+    //methode save an attack
+    @Transactional
+    public Attack save(Attack attack) {
+        return attackDao.save(attack);
+    }
+
+    //methode find an attack by id
+    public Optional<Attack> findById(Long id) {
+        return attackDao.findById(id);
+    }
+
+    //find all attacks
+    public List<Attack> findAll() {
+        return attackDao.findAll();
+    }
+
+    //delete an attack by id
+    @Transactional
+    public void deleteById(Long id) {
+        attackDao.deleteById(id);
+    }
+
 }
