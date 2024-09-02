f89eb8eeec033ff760b196e2708e7513464d165c
amir <amirben1991@gmail.com>
2024-09-02 21:48:07 +0200

    commit de précaution, en cas d'oubli

--- src/main/java/tp_group1/spring_boot_pokemon/dao/InventoryDao.java
+++ src/main/java/tp_group1/spring_boot_pokemon/dao/InventoryDao.java
@@ -1,12 +1,17 @@
 package tp_group1.spring_boot_pokemon.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 import tp_group1.spring_boot_pokemon.model.Inventory;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface InventoryDao extends CrudRepository<Inventory, Long> {
-    List<Inventory> findTrainerById(Long trainerId);
+    List<Inventory> findTrainerById(Long id);
+    Optional<Inventory> findByTrainerId(Long id);
+    List<Inventory> findAll();
+
 }
--- src/main/java/tp_group1/spring_boot_pokemon/model/Item.java
+++ src/main/java/tp_group1/spring_boot_pokemon/model/Item.java
@@ -1,11 +1,14 @@
 package tp_group1.spring_boot_pokemon.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Item {
 
 
