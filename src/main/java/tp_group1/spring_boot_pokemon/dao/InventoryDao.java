77f52290ea79d5cc08c92c353864eba548f59782
Muriel <muriel.marchal@hotmail.com>
2024-09-03 07:23:01 +0200

    modif inventory dao

--- src/main/java/tp_group1/spring_boot_pokemon/dao/InventoryDao.java
+++ src/main/java/tp_group1/spring_boot_pokemon/dao/InventoryDao.java
@@ -1,18 +1,15 @@
 package tp_group1.spring_boot_pokemon.dao;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 import tp_group1.spring_boot_pokemon.model.Inventory;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface InventoryDao extends JpaRepository<Inventory, Long> {
+public interface InventoryDao extends CrudRepository<Inventory, Long> {
     List<Inventory> findTrainerById(Long id);
-    Optional<Inventory> findByTrainerId(Long id);
+
     List<Inventory> findAll();
 
 }
